ef0a49df0d3e35040a53c23f14b7a61d7cf77995
Matias Reyna <matiireyna@hotmail.com>
2024-04-03 11:13:19 -0300

    Mejoras en el modelo: turno

--- api/src/models/turno.js
+++ api/src/models/turno.js
@@ -4,14 +4,24 @@ module.exports = (sequelize) => {
     sequelize.define('turno', {
         day: {
             type: DataTypes.DATEONLY,  // Este es solo para almacener la fecha en la base de datos.
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isDate: true,  // Verifica que la fecha tenga el formato correcto.
+                isAfter: new Date().toISOString().split('T')[O]  // Garantiza que la fecha sea igual o posterior al día actual.
+            }
         },
         timetable: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: false,
             validate: {
-                is: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/  // Validación del formato hora HH:MM.
+                is: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/,  // Validación del formato hora HH:MM.
+                isValidTime(value) {  // Verifica que el horario este dentro de un rango válido (00:00 a 23:59).
+                    const [ hour, minute ] = value.split(':');
+                    if ((parseInt(hour) < 0 || parseInt(hour) > 23 || parseInt(minute) < 0) || parseInt(minute) > 59) {
+                        throw Error('Invalid time')
+                    }
+                }
             }
         },
         states: {
