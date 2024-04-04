0354d84d7cc2e04cbbe922bc6c37cd3b2be669b9
Matias Reyna <matiireyna@hotmail.com>
2024-04-04 09:50:04 -0300

    Configuracion de rutas: user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,11 @@ const mainRouter = Router();
 // Aca van a estar las route's.
 const serviceRouter = require('./serviceRouter');
 const turnoRouter = require('./turnoRouter');
+const userRouter = require('./userRouter');
 
 // Entry points.
 mainRouter.use('/service', serviceRouter);
 mainRouter.use('/turno', turnoRouter);
+mainRouter.use('/user', userRouter);
 
 module.exports = mainRouter
